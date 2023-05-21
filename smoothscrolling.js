a52f73379e6148b5fe46b1bcf217c06a65a6bdaf
c-eric <43305167+c-eric@users.noreply.github.com>
2023-05-20 20:14:08 -0400

    Update smoothscrolling.js

--- smoothscrolling.js
+++ smoothscrolling.js
@@ -1,1 +1,8 @@
+const navigation = document.querySelector(".navbar");
 
+const navigationHeight = navigation.offsetHeight;
+
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navigationHeight + "px"
+);
